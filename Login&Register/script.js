817157943faa124e9ced782dd3c6643e611011c4
domen.magnor@gmail.com <domen.magnor@gmail.com>
2020-10-24 00:39:44 +0200

    Integracija Anine resitve za registracijo in vpisovanje

--- Login&Register/script.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $(".hamburger .fa").click(function(){
-        $(".wrapper").addClass("active")
-    })
-
-    $(".wrapper .sidebar .close").click(function(){
-        $(".wrapper").removeClass("active")
-    })
-});
